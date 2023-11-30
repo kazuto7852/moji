f17161b0bf34e2c0101f6211d6f665cdebaa1890
kazuto Hongou <KaZuto7852@outlook.jp>
2023-11-30 16:50:45 +0900

    make function

--- main.cpp
+++ main.cpp
@@ -1,22 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-
-int main() {
-	const int col = 8;
-	const int row = 8;
-
-	FILE* fp = fopen("data.txt", "r");
-
-	char data[row][col + 1] = {};
+#include<assert.h>
+void scanFile(FILE* fp,char d[8][9], int row, int col) {
 	int r = 0;
-	while (fscanf(fp, "%s", data[r]) != EOF) {
+	while (fscanf(fp, "%s", d[r]) != EOF) {
 		r++;
 	}
+}
 
-	fclose(fp);
+void makeF(char d[8][9],int row,int col) {
 	for (int r = 0; r < row; r++) {
 		for (int c = 0; c < col; c++) {
-			if (data[r][c] == '1') {
+			if (d[r][c] == '1') {
 				printf("*");
 			}
 			else {
@@ -25,6 +20,37 @@ int main() {
 		}
 		printf("\n");
 	}
+	printf("\n");
+}
+
+void revolution(char d[8][9], int row, int col) {
+	for (int r = 0; r < row; r++) {
+		for (int c = 0; c < col; c++) {
+			if (d[r][c] == '1') {
+				printf("*");
+			}
+			else {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main() {
+	const int col = 8;
+	const int row = 8;
+
+	FILE* fp = fopen("data.txt", "r");
+	char data[row][col + 1] = {};
+
+	scanFile(fp,data,row,col);
+
+	fclose(fp);
+
+	makeF(data, row, col);
+
+	revolution(data, row, col);
 
 	return 0;
 }
